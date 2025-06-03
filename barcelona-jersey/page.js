15a37bbf9addb944a0708959b3ee31dc8ad02897
Joyel-Fernandes <fer.joyel@gmail.com>
2025-06-03 17:55:17 +0530

    Update page.js

--- barcelona-jersey/page.js
+++ barcelona-jersey/page.js
@@ -1,14 +1,14 @@
 function changeImage1(){
     const mainImg = document.getElementById ("current-img")
-    mainImg.src = "../real-madrid-jersey/image/firstImg.webp" ;
+    mainImg.src = "../barcelona-jersey/image/firstImg.webp" ;
 }
 
 function changeImage2(){
     const mainImg = document.getElementById ("current-img")
-    mainImg.src = "../real-madrid-jersey/image/secondImg.webp" ;
+    mainImg.src = "../barcelona-jersey/image/secondImg.webp" ;
 }
 
 function changeImage3(){
     const mainImg = document.getElementById ("current-img")
-    mainImg.src = "../real-madrid-jersey/image/thirdImg.webp" ;
+    mainImg.src = "../barcelona-jersey/image/thirdImg.webp" ;
 }
