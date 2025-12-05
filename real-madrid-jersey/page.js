e0503119e3bd9c9c680adb8f7f8496078de744c8
Joyel-Fernandes <fer.joyel@gmail.com>
2025-12-05 17:12:52 +0530

    Color Change Commit

--- real-madrid-jersey/page.js
+++ real-madrid-jersey/page.js
@@ -12,3 +12,16 @@ function changeImage3(){
     const mainImg = document.getElementById ("current-img")
     mainImg.src = "/real-madrid-jersey/image/thirdImg.webp" ;
 }
+
+
+
+
+
+
+function openPopup() {
+    document.getElementById("popupOverlay").style.display = "flex";
+  }
+
+  function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+  }
